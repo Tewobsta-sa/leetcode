17e496c4be22581134be62ff1abcfb0eb37693da
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2024-04-03 14:21:00 +0300

    Update Maximum Ice Cream Bars.py

--- Maximum Ice Cream Bars.py
+++ Maximum Ice Cream Bars.py
@@ -8,3 +8,20 @@ class Solution:
             if cost <= coins:
                counter += 1
         return counter
+#we can work the sorting using the counting sort algorithm here is my code:
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        
+        count=[0]*(max(costs)+1)
+        for num in costs:
+            count[num]+=1
+        sorted_array = []
+        for i in range(len(count)):
+            sorted_array.extend([i]*count[i])
+        cost = 0
+        counter = 0
+        for i in range(len(sorted_array)):
+            cost += sorted_array[i]
+            if cost <= coins:
+               counter += 1
+        return counter
